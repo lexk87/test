68f2f1030ef56cc874202a07b510ada9e055510d
Alexandr Katyshev <katyshev1@gmail.com>
2022-02-16 11:37:38 +0200

    next commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$("#cow").mouseenter(function(){
+        $("<audio></audio>").attr({
+                'src':'../audio/cow-sound.mp3',
+                'volume':1,
+                'autoplay':'autoplay'
+        }).appendTo("body");
+});
